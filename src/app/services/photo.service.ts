e50efa2f677c848db22db2f6f8471920c35300c5
Alexander Runge <alexander@khrunge.dk>
2026-01-15 11:05:26 +0100

    rename project and folder to follow angular file naming convention

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotoService {
+  public async addNewToGallery() {
+
+  }
+}
