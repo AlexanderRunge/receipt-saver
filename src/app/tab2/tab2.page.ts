d8c9a608d7c29b05435aa6da101d4b1c57e757cd
Alexander Runge <alexander@khrunge.dk>
2026-01-17 16:06:52 +0100

    photo service and gallery feature complete

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon} from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, inject } from '@angular/core';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg,
+  ActionSheetController
+} from '@ionic/angular/standalone';
+import type { UserPhoto } from '../services/photo.service'
+import { PhotoService } from '../services/photo.service';
 import { addIcons } from "ionicons";
 import { camera } from 'ionicons/icons';
 
@@ -9,12 +13,45 @@ import { camera } from 'ionicons/icons';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg]
 })
+
 export class Tab2Page {
+  public photoService = inject(PhotoService);
+  public actionSheetController = inject(ActionSheetController);
 
   constructor() {
     addIcons({ camera });
   }
 
+  async ngOnInit() {
+    await this.photoService.loadSaved()
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            this.photoService.deletePhoto(photo, position);
+          },
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {},
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
 }
